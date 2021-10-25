3144b06e9f3cc70d6900fb317179c4f793a11aa8
elineri <elin.ericstam@gmail.com>
2021-10-25 11:11:51 +0200

    Justerat formlerna eftersom de inte stämde. Några andra småjusteringar.

--- CircleArea/Program.cs
+++ CircleArea/Program.cs
@@ -3,103 +3,114 @@ using System;
 
 namespace CircleArea
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Circle 1
-            Circle circle1 = new Circle(); 
-            circle1.Radius = 5;
-
-            Console.WriteLine("\nCirkel 1: ");
-            circle1.GetArea();
-            circle1.GetPerimeter();
-            circle1.GetVolume();
-
-            // Circle 2
-            Circle circle2 = new Circle();
-            circle2.Radius = 6;
-
-            Console.WriteLine("\nCirkel 2: ");
-            circle2.GetArea();
-            circle2.GetPerimeter();
-            circle2.GetVolume();
-
-            //Triangle 1
-            Triangle triangle1 = new Triangle();
-            triangle1.Height = 5;
-            triangle1.Base = 3;
-
-            Console.WriteLine("\nTriangel 1: ");
-            triangle1.GetArea();
-            triangle1.GetPerimeter();
-            triangle1.GetVolume();
-        }
-    }
-
     public class Circle
     {
-        public float Pi = 3.141f;
-        public int Radius;
+        public float _pi = 3.141f;
+        public int _radius;
 
-        public void GetArea() // Calculates and prints area of a circle
+        public Circle(int Radius) // Constructor
         {
-            float area = Radius * Radius * Pi;
+            this._radius = Radius;
+        }
+
+        public float GetArea() // Calculates and prints area of a circle
+        {
+            float area = _radius * _radius * _pi;
 
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine($"Arean för en cirkel med radien {Radius} är: {area}");
+            Console.WriteLine($"Arean för cirkeln är: {area}");
             Console.ForegroundColor = ConsoleColor.Gray;
+
+            return area;
         }
 
         public void GetPerimeter() // Calculates and prints perimeter of a circle
         {
-            float perimeter = 2 * Pi * Radius;
+            float perimeter = 2 * _pi * _radius;
 
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine($"Omkretsen för en cirkel med radien {Radius} är: {perimeter}");
+            Console.WriteLine($"Omkretsen cirkeln är: {perimeter}");
             Console.ForegroundColor = ConsoleColor.Gray;
         }
 
-        public void GetVolume() // Calculates and prints volume of a sphere
+        public float GetVolume() // Calculates and prints volume of a sphere
         {
-            float volume = (4 * Pi * (Radius * 3)) / 3;
+            float volume = (4 * _pi * (_radius * _radius * _radius)) / 3;
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"Volymen för en sfär med radien {Radius} är: {volume}");
+            Console.WriteLine($"Volymen sfären är: {volume}");
             Console.ForegroundColor = ConsoleColor.Gray;
+
+            return volume;
         }
     }
 
     public class Triangle
     {
-        public int Height;
-        public int Base;
+        public int _height;
+        public int _base;
 
-        public void GetArea() // Calculates and prints area of a triangle
+        public Triangle(int Height, int Base) // Constructor
         {
-            double area = (Height * Base) / 2;
+            this._height = Height;
+            this._base = Base;
+        }
+
+        public double GetArea() // Calculates and prints area of a triangle
+        {
+            double area = (_height * _base) / 2;
 
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine($"Area för en triangel med basen {Base} och höjden {Height} är: {area}");
+            Console.WriteLine($"Arean för triangeln är: {area}");
             Console.ForegroundColor = ConsoleColor.Gray;
+
+            return area;
         }
 
-        public void GetPerimeter() // Calculates and prints perimeter of a triangle
+        public double GetPerimeter() // Calculates and prints perimeter of a triangle
         {
-            double perimeter = ((Height * 2) + (Base * 2)) / 2;
+            double perimeter = ((_height * 2) + (_base * 2)) / 2;
 
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine($"Omkretsen för en triangel med basen {Base} och höjden {Height} är: {perimeter}");
+            Console.WriteLine($"Omkretsen för triangeln är: {perimeter}");
             Console.ForegroundColor = ConsoleColor.Gray;
+
+            return perimeter;
         }
 
-        public void GetVolume() // Calculates and prints volume of a pyramid
+        public double GetVolume() // Calculates and prints volume of a pyramid
         {
-            double volume = (((Height * Base) / 2) * Height) / 3;
+            double volume = (_height * _base) / 3;
 
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"Volymen för pyramiden är: {volume}");
             Console.ForegroundColor = ConsoleColor.Gray;
+
+            return volume;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Circle 1
+            Circle c1 = new Circle(5); 
+
+            Console.WriteLine("\nCirkel 1: ");
+            c1.GetArea(); c1.GetPerimeter(); c1.GetVolume();
+
+            // Circle 2
+            Circle c2 = new Circle(6);
+
+            Console.WriteLine("\nCirkel 2: ");
+            c2.GetArea(); c2.GetPerimeter(); c2.GetVolume();
+
+            //Triangle 1
+            Triangle t1 = new Triangle(5, 3);
+
+            Console.WriteLine("\nTriangel 1: ");
+            t1.GetArea(); t1.GetPerimeter(); t1.GetVolume();
         }
     }
 }
